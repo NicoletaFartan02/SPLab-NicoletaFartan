a46a9d7c9b1eaa93776bbe031515eb70c90cc88e
NicoletaFartan02 <nicoleta.fartan02@e-uvt.ro>
2023-10-10 08:47:25 +0300

    primul commit

---
+++ src/test/java/com/example/spproject/SpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
